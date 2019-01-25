3f51cfc438a60c4d84e28760a10f8c3916701af7
gonzalo <gonzalo_lledo@outlook.com>
2019-01-25 13:08:03 +0100

    animacion Logo prueba

--- app/src/main/java/com/example/clearsand/MainActivity.java
+++ app/src/main/java/com/example/clearsand/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.clearsand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void prueba(View v){
+        Intent i = new Intent(this,SplashScreenActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/clearsand/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.clearsand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    TextView myTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        myTitle = findViewById(R.id.txtNombreSplashScreen);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+
+        myTitle.startAnimation(myanim);
+    }
+}
