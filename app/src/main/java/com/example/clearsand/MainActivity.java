1863b467fba3da9b52e96f1632fe9b6fae6ad10f
jorge <21746516@live.uem.es>
2019-01-25 09:39:09 +0100

    Prueba

--- app/src/main/java/com/example/clearsand/MainActivity.java
+++ app/src/main/java/com/example/clearsand/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String test;
-
+    String pruebaLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
