ce2c21d07343533841f4eebed21a3972681735d4
21752434 <javiermiok@gmail.com>
2019-02-01 09:37:40 +0100

    ventanaLogin Javier Merge con master para añadir paleta de colores Quitado appbar

--- app/src/main/java/com/example/clearsand/LoginActivity.java
+++ app/src/main/java/com/example/clearsand/LoginActivity.java
@@ -9,5 +9,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Esconde la AppBar
+        getSupportActionBar().hide();
     }
 }
