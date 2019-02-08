1f50e6dd5a943ac81ecc09bc79305c14a6733d59
gonzalo <gonzalo_lledo@outlook.com>
2019-02-08 13:44:00 +0100

    scroll screen xml done

--- app/src/main/java/com/example/clearsand/ScrollingActivity.java
+++ app/src/main/java/com/example/clearsand/ScrollingActivity.java
@@ -6,8 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    EditText etNombrePlaya;
+    EditText etTituloEvento;
+    EditText etHorarioEvento;
+    EditText etFechaEvento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
