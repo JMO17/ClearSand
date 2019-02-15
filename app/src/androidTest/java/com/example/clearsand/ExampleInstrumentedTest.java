0824ef27d6ab106264823e8039c16c3cfaa9277f
21752434 <javiermiok@gmail.com>
2019-02-15 09:23:46 +0100

    Test de Login

--- app/src/androidTest/java/com/example/clearsand/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/clearsand/ExampleInstrumentedTest.java
@@ -1,26 +1,53 @@
 package com.example.clearsand;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @author ernesto
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.clearsand", appContext.getPackageName());
+public class ExampleInstrumentedTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    private Button mloginbutton;
+    private EditText etext1;
+    private EditText etext2;
+
+    public ExampleInstrumentedTest() {
+        super(LoginActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        LoginActivity actividad = getActivity();
+        etext1 = actividad.findViewById(R.id.etEmailLogin);
+        etext2 = actividad.findViewById(R.id.etPasswordLogin);
+        mloginbutton = actividad.findViewById(R.id.btnSignInLogin);
+
+    }
+
+//    protected void tearDown() throws Exception {
+//        super.tearDown();
+//    }
+
+    private static final String USERNAME = "ernesto";
+    private static final String PASSWORD = "123456";
+
+    public void testLoginSignup() {
+//        onView(withId(R.id.email)).perform(typeText(USERNAME));
+//        onView(withId(R.id.password)).perform(typeText(PASSWORD));
+
+        // first tap on value1 entry
+        TouchUtils.tapView(this, etext1);
+        getInstrumentation().sendStringSync(USERNAME);
+        // now on value2 entry
+        TouchUtils.tapView(this, etext2);
+        getInstrumentation().sendStringSync(PASSWORD);
+        // now on login button
+        TouchUtils.tapView(this, mloginbutton);
+
     }
 }
