3634062bf3498471d007ae634d65571670e09902
gonzalo <gonzalo_lledo@outlook.com>
2019-02-22 13:00:34 +0100

    mapa activity funcionaaaa

--- app/src/main/java/com/example/clearsand/PerfilPlayaActivity.java
+++ app/src/main/java/com/example/clearsand/PerfilPlayaActivity.java
@@ -3,14 +3,58 @@ package com.example.clearsand;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PerfilPlayaActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PerfilPlayaActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_playa);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         getSupportActionBar().hide();
+
+
+
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);//Imágenes satelite
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Add a marker in Madrid and move the camera
+        LatLng mad = new LatLng(40, -3);
+        mMap.addMarker(new MarkerOptions().position(mad).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mad));
+
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(new LatLng(40, -4))
+                .zoom(5)
+                .build();
+        CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+        mMap.animateCamera(camUpd3);
+
+
+
+    }
+
+
 }
