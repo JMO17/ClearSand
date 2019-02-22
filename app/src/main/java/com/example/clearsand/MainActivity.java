95cc0e7e9f22703b24f46dfdc494788b912de29d
jorge <21746516@live.uem.es>
2019-02-22 11:01:50 +0100

    creation abstract main menu

--- app/src/main/java/com/example/clearsand/MainActivity.java
+++ app/src/main/java/com/example/clearsand/MainActivity.java
@@ -1,19 +1,57 @@
 package com.example.clearsand;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     String test;
     String SecondMerge;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.nav_eventos:
+                    Toast.makeText(MainActivity.this, "Eventos", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.nav_explorar:
+                    Toast.makeText(MainActivity.this, "explorar", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.nav_favoritos:
+                    Toast.makeText(MainActivity.this, "favoritos", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.nav_perfil:
+                    Toast.makeText(MainActivity.this, "perfil", Toast.LENGTH_LONG).show();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public abstract int cargarLayout();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View v = findViewById(R.id.relative);
+        RelativeLayout rel = (RelativeLayout) v;
+        getLayoutInflater().inflate(cargarLayout(),rel);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
     }
