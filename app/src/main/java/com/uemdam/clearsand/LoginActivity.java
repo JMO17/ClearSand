227d858f76c84a87b41bf8b391fc3aa8b7237dbd
21752434 <javiermiok@gmail.com>
2019-02-22 14:01:07 +0100

    Actualizado Cambiado imagen de login(revisar) Quitado status bar

--- app/src/main/java/com/uemdam/clearsand/LoginActivity.java
+++ app/src/main/java/com/uemdam/clearsand/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.uemdam.clearsand;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -35,8 +37,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        /*Esconder la AppBar*/
-        getSupportActionBar().hide();
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            /*Esconder la AppBar*/
+            getSupportActionBar().hide();
+        }
+
+
 
         /* Inicialización de atributos*/
         fba = FirebaseAuth.getInstance();
