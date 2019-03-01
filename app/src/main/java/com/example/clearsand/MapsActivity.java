63f8d6930dda32471a80e2379dfa7ec6bfeac984
gonzalo <gonzalo_lledo@outlook.com>
2019-03-01 09:54:25 +0100

    mapas

--- app/src/main/java/com/example/clearsand/MapsActivity.java
+++ app/src/main/java/com/example/clearsand/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.clearsand;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.clearsand.javabean.Playa;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,14 +11,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private ArrayList<Playa> playas;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
