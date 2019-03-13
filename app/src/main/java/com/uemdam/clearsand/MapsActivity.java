008078e4f1f86adcddc89337fdaf143dbb95d3e5
gonzalo <gonzalo_lledo@outlook.com>
2019-03-13 13:07:20 +0100

    playa

---
+++ app/src/main/java/com/uemdam/clearsand/MapsActivity.java
@@ -0,0 +1,154 @@
+package com.uemdam.clearsand;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.uemdam.clearsand.javabean.Playa;
+
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private ArrayList<Playa> datosPlayas;
+    private GoogleMap mMap;
+
+    /*DATABASE*/
+    private DatabaseReference dbR;
+    private ChildEventListener cel;
+
+    private int id;
+    private  Playa playa;
+
+    BitmapDescriptor icon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        datosPlayas = new ArrayList<>();
+
+        /*DATABASE*/
+        dbR = FirebaseDatabase.getInstance().getReference().child("PLAYAS");
+
+        icon = generateBitmapDescriptor(this, R.drawable.ic_swimming_zone_marker);
+
+        addChildEventListener();
+
+        id = getIntent().getIntExtra("IDMAPA", 0);
+        playa = null;
+
+        for (Playa com : datosPlayas) {
+            if (Integer.parseInt(com.getId()) == id) {
+                playa = com;
+            }
+        }
+
+    }
+
+    private void addChildEventListener() {
+        if (cel == null) {
+            cel = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Playa m = dataSnapshot.getValue(Playa.class);
+                    datosPlayas.add(m);
+                    LatLng gms = new LatLng(Double.parseDouble(m.getCoordenada_X()), Double.parseDouble(m.getCoordenada_Y()));
+
+                    mMap.addMarker(new MarkerOptions().position(gms).title(m.getNombre())).setIcon(icon);
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            dbR.addChildEventListener(cel);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //BitmapDescriptor icon = generateBitmapDescriptor(this, R.drawable.ic_swimming_zone_marker);
+
+
+        if (playa == null) {
+          /*  for (Playa plas : datosPlayas) {
+                LatLng gms = new LatLng(Double.parseDouble(plas.getCoordenada_X()), Double.parseDouble(plas.getCoordenada_Y()));
+
+                mMap.addMarker(new MarkerOptions().position(gms).title(plas.getNombre())).setIcon(icon);
+            }*/
+            LatLng gmsAux = new LatLng(-40,3.7);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(gmsAux));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(9));
+        } else {
+
+            LatLng localizacionPlaya = new LatLng(Double.parseDouble(playa.getCoordenada_X()), Double.parseDouble(playa.getCoordenada_Y()));
+            mMap.addMarker(new MarkerOptions().position(localizacionPlaya).title(playa.getNombre())).setIcon(icon);
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(localizacionPlaya));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+
+        }
+
+
+    }
+
+    public static BitmapDescriptor generateBitmapDescriptor(Context context, int resId) {
+        Drawable drawable = ContextCompat.getDrawable(context, resId);
+        drawable.setBounds(
+                0,
+                0,
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+}
---
+++ app/src/main/java/com/uemdam/clearsand/PerfilPlayaActivity.java
@@ -0,0 +1,172 @@
+package com.uemdam.clearsand;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.uemdam.clearsand.javabean.Playa;
+
+import java.util.ArrayList;
+
+public class PerfilPlayaActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    //Mapa
+    private GoogleMap mMap;
+
+    //DATABASE
+    private DatabaseReference dbR;
+    private ChildEventListener cel;
+
+    private Playa playa;
+    private ArrayList<Playa> datoPlaya;
+    private int id;
+    private double latitud;
+    private double longitud;
+    private String nombre;
+    TextView tvNombre;
+    ImageView ivPlaya;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_playa2);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        getSupportActionBar().hide();
+
+        id = getIntent().getIntExtra("CLAVEINTENT",0);
+        datoPlaya = new ArrayList<>();
+        //DATABASE
+        dbR = FirebaseDatabase.getInstance().getReference().child("PLAYAS");
+
+        addChildEventListener();
+
+        tvNombre = findViewById(R.id.tvNombrePlayaPerfilActivity);
+        ivPlaya = findViewById(R.id.ivPerfilPLayaActivity);
+
+        nombre = datoPlaya.get(0).getNombre();
+        Glide.with(ivPlaya.getContext()).load(datoPlaya.get(0).getUrlImagen()).into(ivPlaya);
+        tvNombre.setText(nombre);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);//Imágenes satelite
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        BitmapDescriptor icon = generateBitmapDescriptor(this, R.drawable.ic_swimming_zone_marker);
+
+
+        latitud = Double.parseDouble(datoPlaya.get(0).getCoordenada_X());
+        longitud = Double.parseDouble(datoPlaya.get(0).getCoordenada_Y());
+        LatLng mad = new LatLng(latitud, longitud);
+
+        mMap.addMarker(new MarkerOptions().position(mad).title(datoPlaya.get(0).getNombre()).icon(icon));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mad));
+
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(new LatLng(40, -4))
+                .zoom(7)
+                .build();
+        CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+        mMap.animateCamera(camUpd3);
+
+
+
+    }
+
+    public void crearEvento(View v){
+
+    }
+
+    public void comoLlegar(View v){
+        Intent i = new Intent(this,MapsActivity.class);
+        i.putExtra("IDMAPA",Integer.parseInt(playa.getId()));
+        startActivity(i);
+    }
+
+    public static BitmapDescriptor generateBitmapDescriptor(Context context, int resId) {
+        Drawable drawable = ContextCompat.getDrawable(context, resId);
+        drawable.setBounds(
+                0,
+                0,
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    private void addChildEventListener() {
+        if (cel == null) {
+            cel = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Playa m = dataSnapshot.getValue(Playa.class);
+                    if(Integer.parseInt(m.getId())== id) {
+                        datoPlaya.add(m);
+                    }
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            dbR.addChildEventListener(cel);
+        }
+    }
+}
