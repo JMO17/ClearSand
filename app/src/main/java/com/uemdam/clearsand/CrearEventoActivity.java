73afc3d806cf67ba4938c13aaddfa4c4efaba03d
gonzalo <gonzalo_lledo@outlook.com>
2019-03-13 11:48:32 +0100

    evento

---
+++ app/src/main/java/com/uemdam/clearsand/CrearEventoActivity.java
@@ -0,0 +1,13 @@
+package com.uemdam.clearsand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_eventoo);
+    }
+}
